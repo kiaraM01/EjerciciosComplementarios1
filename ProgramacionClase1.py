48b5ee90325403aa3c0b0ab5105f86ef2dfffe21
jakimo <kiamoyano02@gmail.com>
2023-10-01 20:47:42 -0300

    x

---
+++ ProgramacionClase1.py
@@ -0,0 +1,52 @@
+#Ejercicio 1
+numero1=30
+#Ejercicio 2
+numero2=5.5
+#Ejercicio 3
+suma=numero1+numero2
+print("Suma: ", suma)
+#Ejercio 4
+resta=numero1-numero2
+multi=numero1*numero2
+div=numero1/numero2
+print(f"Resta: {resta}; Multiplicacion: {multi}; Division: {div}")
+#Ejercicio 5 y 6
+nombre="Pan"
+precio=10.5
+print(f"Precio del {nombre}: {precio}")
+#Ejercicio 7 y 8
+descuento=0.15
+precio_final=int(precio-(precio*descuento))
+print(f"precio del {nombre} con descuento: ${precio_final}")
+#Ejercicio 9 y 10
+cadena="cadena de texto"
+longitud=len(cadena)
+print(f"La cadena: '{cadena}' tiene {longitud} palabas")
+#Ejercicio 11
+precio=55.5
+precio=int(precio)
+print(f"valor entero: {precio}")
+#Ejercicio 12
+nombre="Juan"
+apellido="Perez"
+nombre_completo=nombre+" "+apellido
+print(f"Su nombre es: {nombre_completo}")
+#Ejecicio 13
+edad=22
+edad=edad+5
+edad=edad-10
+print("Edad: ",edad)
+#Ejercicio 14
+altura=1.59
+altura=altura*4
+altura=altura-10
+#Ejercicio 15
+mi_nombre_completo="CHIARA MOYANO"
+print("Nombre: ",mi_nombre_completo)
+mi_nombre_completo=mi_nombre_completo.lower()
+print("Su nombre es: ",mi_nombre_completo)
+#Ejercicio 16
+mi_nombre_completo=mi_nombre_completo.upper()
+print("Su nombre: ",mi_nombre_completo)
+mi_nombre_completo=mi_nombre_completo.capitalize()
+print("Su nombre (m): ",mi_nombre_completo)
